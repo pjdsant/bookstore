53b2680d0cd8d0a4821c3af52ff1acb626e2cff1
unknown <pjdsant@gmail.com>
2019-12-16 21:02:58 -0300

    first commit

---
+++ BookStore.Domain/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore.Domain
+{
+    public class Categoria
+    {
+        //Necessario para iniciar a colecao de Livros
+        public Categoria()
+        {
+            this.Livros = new List<Livro>();
+        }
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public ICollection<Livro> Livros { get; set; }
+    }
+}
